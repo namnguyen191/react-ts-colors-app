5151534d02df095a85f50c8099528efc9ee63e4a
= <=>
2020-12-25 14:58:39 -0500

    Style color box

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Pallete from './Pallete';
 import seedColors from './seedColors';
+import './App.css';
 
 function App() {
   return (
--- src/ColorBox.tsx
+++ src/ColorBox.tsx
@@ -8,10 +8,17 @@ interface ColorBoxProps {
 
 class ColorBox extends Component<ColorBoxProps> {
   render() {
+    const { name, color } = this.props.color;
+
     return (
-      <div className="ColorBox" style={{ background: this.props.color.color }}>
-        <span>{this.props.color.name}</span>
-        <span>MORE</span>
+      <div className="ColorBox" style={{ background: color }}>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
       </div>
     );
   }
